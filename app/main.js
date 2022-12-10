918506b18dbf3bbba89c55150a7bff43c364bf4c
jobebe07 <89039564+jobebe07@users.noreply.github.com>
2022-12-10 22:05:10 +0100

    layout_13

--- app/main.js
+++ app/main.js
@@ -1,3 +1,3 @@
-import TicTacToeField from "./modules/TicTacToeField";
+import TicTacToeField from "./modules/TicTacToeField.js";
 
 let field = new TicTacToeField(document.getElementsByClassName("grid-main")[0])
