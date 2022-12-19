7ddb11d7529d36db46ceb81613b9ba84d67eec7c
jobebe07 <johannesbecker514@gmail.com>
2022-12-19 12:59:28 +0000

    added hasParent helper function, added favicon

--- app/main.js
+++ app/main.js
@@ -1,9 +1,14 @@
 import TicTacToeField from "./modules/TicTacToeField.js";
+import hasParent from "./modules/utils.js";
 
 window.addEventListener("load", () => {
     let field = new TicTacToeField(document.getElementsByClassName("grid-main")[0])
     field.updateGrid()
     window.field = field
+    window.hasParent = hasParent
 })
 
 
+window.addEventListener("click", (e) => {
+
+})
