b95ee79195a86699c27bea8d31be902452f686a1
jobebe07 <89039564+jobebe07@users.noreply.github.com>
2022-12-20 10:13:16 +0000

    test_09 fix

--- app/main.js
+++ app/main.js
@@ -17,7 +17,7 @@ window.addEventListener("click", (e) => {
         let gridChords = field.numToChords(result.fieldId)
         let itemChords = field.numToChords(result.itemId)
 
-        if(field.getItem(gridChords.row, gridChords.col, itemChords.row, itemChords.col) != Items.DEFAULT) {
+        if(field.getItem(gridChords.row, gridChords.col, itemChords.row, itemChords.col) == Items.DEFAULT) {
             field.setItem(gridChords.row, gridChords.col, itemChords.row, itemChords.col, current)
             if(current == Items.CROSS) current = Items.CIRCLE
             else if(current == Items.CIRCLE) current = Items.CROSS
