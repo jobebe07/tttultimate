2e5bd70284f5a529353e3fd2db58ec62beb86ece
jobebe07 <johannesbecker514@gmail.com>
2023-01-08 16:07:27 +0100

    i gave up the ssl test

--- app/main.js
+++ app/main.js
@@ -45,7 +45,7 @@ function setOnlineGame() {
     } catch (e) {
 
     }
-    websocket = new WebSocket("wss://ttt.belinked.chat:443")
+    websocket = new WebSocket("ws://ttt.belinked.chat:8080")
     field = new TicTacToeField(document.getElementsByClassName("grid-main")[0])
     field.resetField()
     window.field = field
